0418836d165bf0833104e3f828516c871c64bb0c
ogagaoghene emmanuel <esavwede84@gmail.com>
2025-04-04 18:08:06 +0100

    chore(crop-recommendation): fixed format

--- src/plant-recommendations/controllers/plant-recommendation.controller.ts
+++ src/plant-recommendations/controllers/plant-recommendation.controller.ts
@@ -1,41 +1,30 @@
+import { NextFunction, Request, Response } from "express";
 
-import { NextFunction, Request, Response} from "express"
+import { PlantRecommendationService } from "../services/plant-recommendation.service";
+import { FarmDataInput } from "../schemas/plant-recommendation.schema";
 
-import { PlantRecommendationService } from "../services/plant-recommendation.service"
-import { FarmDataInput } from "../schemas/plant-recommendation.schema"
+export default class PlantRecommendationController {
+  private plantRecommendationService: PlantRecommendationService;
 
+  constructor() {
+    this.plantRecommendationService = new PlantRecommendationService();
+  }
 
-export default class PlantRecommendationController
-{
+  async recommendCrops(
+    req: Request<{}, {}, FarmDataInput["body"]>,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      res.setTimeout(100000);
+      const farmData = req.body;
+      const plantRecommendations =
+        await this.plantRecommendationService.recommendPlant(farmData);
 
-    private plantRecommendationService: PlantRecommendationService
-
-    constructor()
-    {
-        this.plantRecommendationService = new PlantRecommendationService()
+      res.status(200).json({ status: "success", data: [plantRecommendations] });
+      return;
+    } catch (e: any) {
+      next(e);
     }
-
-
-    async recommendCrops
-    (
-        req: Request<{},{}, FarmDataInput['body']>, 
-        res: Response, 
-        next: NextFunction 
-    )
-    {
-        try 
-        {
-            res.setTimeout(100000)
-            const farmData = req.body
-            const plantRecommendations = await this.plantRecommendationService.recommendPlant( farmData ) 
-            
-            res.send( plantRecommendations )
-            return 
-        }
-        catch(e: any)
-        {
-            next(e) 
-        }
-    } 
-
+  }
 }
--- src/plant-recommendations/services/plant-recommendation.service.ts
+++ src/plant-recommendations/services/plant-recommendation.service.ts
@@ -1,72 +1,77 @@
-
-
-import {  config } from "dotenv"
-import { fetchHistoricalWeatherData, predictFutureWeather } from "./weather.service"
-import { GoogleGeminiClient } from "../../../genAI/models/gemini/gemini"
+import { config } from "dotenv";
+import {
+  fetchHistoricalWeatherData,
+  predictFutureWeather,
+} from "./weather.service";
+import { GoogleGeminiClient } from "../../../genAI/models/gemini/gemini";
 import { FarmDataInput } from "../types/farm-data";
-import { extractJsonFromString } from "../../utils/strings/extract-json-from-string" 
+import { extractJsonFromString } from "../../utils/strings/extract-json-from-string";
 
-config()
+config();
 
+export class PlantRecommendationService {
+  private plantRecommendationModel: GoogleGeminiClient;
 
-export class PlantRecommendationService
-{
-    private plantRecommendationModel: GoogleGeminiClient
+  constructor() {
+    this.plantRecommendationModel = new GoogleGeminiClient();
+  }
 
-    constructor()
-    {
-        this.plantRecommendationModel = new GoogleGeminiClient() 
+  async recommendPlant(farmData: FarmDataInput) {
+    try {
+      const { longitude, latitude, area, plants, soilType } = farmData;
+
+      // Fetch Historical Weather Data
+      const historicWeatherData = await fetchHistoricalWeatherData(
+        longitude,
+        latitude
+      );
+
+      // Ensure Data Valid
+      if (!historicWeatherData) {
+        return;
+      }
+
+      // Predict Future Temperature
+      const { predictedTemperature, predictedPrecipitation } =
+        predictFutureWeather(historicWeatherData);
+
+      // Display Predicted Temperature
+      console.log(
+        `The predicted temperature for Long:${longitude} and Lat:${latitude} is: ${predictedTemperature} *C`
+      );
+      console.log(
+        `The predicted precipitation for Long:${longitude} and Lat:${latitude} is: ${predictedPrecipitation} *C`
+      );
+
+      const plantData: FarmDataInput = {
+        latitude,
+        longitude,
+        averageTemperature: predictedTemperature,
+        totalPrecipitation: predictedPrecipitation,
+        plants,
+        soilType,
+        numberOfMonths: 3,
+      };
+
+      // Generate AI Recommendations Based on {User Provided Data} and {Temperature & Precipitation  Data}
+      const plantRecommendations: any = await this.generatePlantRecommendations(
+        plantData
+      );
+
+      var res = extractJsonFromString(plantRecommendations);
+      res = JSON.parse(res);
+
+      return res;
+    } catch (e: any) {
+      console.log("Service: Could Not Recommend Crop to plant");
+      console.log(e);
+      throw e;
     }
+  }
 
-
-    async recommendPlant( farmData: FarmDataInput )
-    {
-        try 
-        {
-            const { longitude, latitude, area, plants, soilType } = farmData
-
-            // Fetch Historical Weather Data 
-            const historicWeatherData = await  fetchHistoricalWeatherData(longitude, latitude)
-            
-            // Ensure Data Valid 
-            if( !historicWeatherData ){ return }
-
-            // Predict Future Temperature 
-            const { predictedTemperature, predictedPrecipitation} = predictFutureWeather( historicWeatherData )
-
-            // Display Predicted Temperature 
-            console.log(`The predicted temperature for Long:${longitude} and Lat:${latitude} is: ${ predictedTemperature } *C`)
-            console.log(`The predicted precipitation for Long:${longitude} and Lat:${latitude} is: ${ predictedPrecipitation} *C`)
-
-            const plantData: FarmDataInput = { latitude, longitude, averageTemperature: predictedTemperature, totalPrecipitation: predictedPrecipitation, plants, soilType, numberOfMonths: 3}
-
-            // Generate AI Recommendations Based on {User Provided Data} and {Temperature & Precipitation  Data}
-            const plantRecommendations:any = await this.generatePlantRecommendations( plantData ) 
-
-
-          
-
-            var res = extractJsonFromString( plantRecommendations)
-            res = JSON.parse( res )
-
-            return res
-        }   
-        catch(e: any)
-        {
-            console.log("Service: Could Not Recommend Crop to plant") 
-            console.log(e) 
-            throw(e) 
-        }
-    }
-
-
-    async generatePlantRecommendations( plantData: FarmDataInput)
-    {
-        try 
-        {
-
-            
-            const CROP_RECOMMENDATION_PROMPT: string = `
+  async generatePlantRecommendations(plantData: FarmDataInput) {
+    try {
+      const CROP_RECOMMENDATION_PROMPT: string = `
 
             SYSTEM: You are an AI system that give's advise and recommendations on the cultivation of a plant. 
 
@@ -80,25 +85,15 @@ export class PlantRecommendationService
 
             You will then analyze the the given data and return a summary, structured as a json response in the form 
             {
-            "climate": {
-                "avgTemperature": "summary of average temperature suitability",
-                "totalPrecipitation": "summary of the total precipitation suitability"
-            },
-            "soil": {
-                "type": "summary of you suggestion for the soil type",
-                "phValue": "summary of the ideal pH range for the plant"
-            },
-            "irrigation": {
-                "technique": " ideal irrigation method summary",
-                "waterQuality": "ideal type of water for the plants based on its constituent",
-                "irrigationSchedule": "ideal irrigation schedule for plant"
-            },
-            "pestAndDiseases": {
-                "commonPests": "common pests that affects plant",
-                "commonPestsMitigation": "mitigation summary",
-                "commonDiseases": " common diseases affecting plant",
-                "commonDiseasesMitigation": "mitigation summary"
-            }
+            "plantName":"name of plant provide",
+            "suitabilityScore": "score between 0 and 100 based on the suitability of the plant for the given data",
+            "growthPeriod": "summary of the growth period of the plant",
+            "averageTemperature": "summary of average temperature suitability",
+            "totalPrecipitation": "summary of the total precipitation suitability",
+            "soilType": "summary of the soil type suitability",
+            "phValue": "summary of the ideal pH range for the plant",
+            "irriagation": "summary of the ideal irrigation method, ideal type of water for the plants based on its constituent, ideal irrigation schedule for plant"
+            "pestAndDiseases": "common pests that affects plant, mitigation summary, common diseases affecting plant, mitigation summary"
             }
 
 
@@ -109,24 +104,23 @@ export class PlantRecommendationService
 
             USER: 
             {
-                "averageTemperature": ${ plantData.averageTemperature }, 
-                "totalPrecipitation": ${ plantData.totalPrecipitation}, 
-                "longitude": ${ plantData.longitude},
-                "latitude": ${ plantData.latitude},
-                "soilType": ${ plantData.soilType}, 
-                "plants": ${ plantData.plants },
-                "numberOfMonths": ${ plantData.numberOfMonths } 
+                "averageTemperature": ${plantData.averageTemperature}, 
+                "totalPrecipitation": ${plantData.totalPrecipitation}, 
+                "longitude": ${plantData.longitude},
+                "latitude": ${plantData.latitude},
+                "soilType": ${plantData.soilType}, 
+                "plants": ${plantData.plants},
+                "numberOfMonths": ${plantData.numberOfMonths} 
             }
-            `
-            const result = await this.plantRecommendationModel.generateRecommendations( CROP_RECOMMENDATION_PROMPT )
-            return result 
-        }
-        catch(e: any)
-        {
-            console.log("Error occured while analyzing plant data")
-            console.log(e) 
-        }
+            `;
+      const result =
+        await this.plantRecommendationModel.generateRecommendations(
+          CROP_RECOMMENDATION_PROMPT
+        );
+      return result;
+    } catch (e: any) {
+      console.log("Error occured while analyzing plant data");
+      console.log(e);
     }
-    
-
+  }
 }
